bd7d7aac8a2d8a184cbf8a4bd9d2ece909ad5276
arthursiufgd10@gmail.com <arthursiufgd10@gmail.com@403bde64-c752-5910-7460-6cf0b36d645b>
2012-02-15 23:19:56 +0000

    Adicionado a classe Execution_HC para executar o código direto pelo Eclipse. Próxima etapa, fazer os métodos de pré processamento.

---
+++ Urocultura/src/core/Execution_HC.java
@@ -0,0 +1,13 @@
+public class Execution_HC {
+
+	public Execution_HC(){
+		Counter_HC counter = new Counter_HC();
+	
+		counter.run(null);
+	}
+	
+	public static void main(String[] args) {
+		Execution_HC h = new Execution_HC();
+	}
+
+}
